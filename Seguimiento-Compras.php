ec720a1ba744e55d00bdb4ca41ef13119edaeafd
unknown <moi.982555147@gmail.com>
2021-12-07 16:39:59 -0500

    se separó paginas en footer y header para ahorro de codigo

---
+++ Seguimiento-Compras.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Meta -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<link rel="shortcut icon" href="assets/images/favicon.png">
+<title>Replay</title>
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+<!-- Bootstrap Core CSS -->
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+<!-- Customizable CSS -->
+<link rel="stylesheet" href="assets/css/main.css">
+<link rel="stylesheet" href="assets/css/blue.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.css">
+<link rel="stylesheet" href="assets/css/owl.transitions.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/rateit.css">
+<link rel="stylesheet" href="assets/css/bootstrap-select.min.css">
+
+<!-- Icons/Glyphs -->
+<link rel="stylesheet" href="assets/css/font-awesome.css">
+<script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script> <!--load all styles -->
+
+<!-- Boxicons -->
+<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Barlow:200,300,300i,400,400i,500,500i,600,700,800" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body class="cnt-home">
+<!-- ============================================== HEADER ============================================== -->
+
+<?php include('header.html'); ?>
+
+<!-- ============================================== HEADER : END ============================================== -->
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="home.html">Inicio</a></li>
+				<li class='active'>Seguimiento de Compras</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content">
+	<div class="container " >
+		<div class="track-order-page">
+			<div class="row">
+				<div class="col-md-12">
+  <div style="background-color: #3D5C98;padding:5%; margin: 0 0 3%;"> 
+    <h2 style="color:#FFF; font-size: 44px;"> <b>SEGUIMIENTO DE COMPRA</b> </h2> 
+  </div>
+	<form class="register-form outer-top-xs" role="form">
+		<div class="form-group " style="margin:1% 25%;">
+		    <label class="info-title" for="exampleOrderId1"><b> Ingresa tu número de pedido </b></label>
+		    <input type="text" class="form-control unicase-form-control text-input" id="exampleOrderId1" style="margin: 4% 0%;border-radius: 15px;border: 1px solid #666666; ">
+        <label class="info-title" for="exampleOrderId1" style="font-size: 12px;">¿Dónde encuentro mi número de pedido? </label>
+        <div style="text-align:center;margin: 4%;">
+          Codigo captcha xd
+        </div>
+		</div>
+
+    <div style="text-align: center; ;">
+      <button type="submit" class="btn-upper btn btn-primary checkout-page-button" style="margin:1% 25%;background-color:#3D5C98;padding: 0.7% 9%"><b>BUSCAR</b></button>
+    </div>
+	 
+	</form>	
+</div>			</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+		<!-- ============================================== BRANDS CAROUSEL ============================================== -->
+<!-- <div id="brands-carousel" class="logo-slider wow fadeInUp">
+
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item m-t-15">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand1.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item m-t-10">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand2.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand3.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand4.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand5.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand6.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand2.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand4.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand1.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="assets/images/brands/brand5.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div>
+		    </div> --><!-- /.owl-carousel #logo-slider -->
+		<!-- </div> --><!-- /.logo-slider-inner -->
+	
+<!-- </div> --><!-- /.logo-slider -->
+<!-- ============================================== BRANDS CAROUSEL : END ============================================== -->	</div><!-- /.container -->
+</div><!-- /.body-content -->
+<!-- ============================================================= FOOTER ============================================================= -->
+
+        <!-- ============================================== INFO BOXES ============================================== -->
+        <div class="row our-features-box" style="margin-top: 2%;">
+     <div class="container" >
+      <ul >
+        <li>
+          <div class="feature-box">
+            <div class="icon-truck"></div>
+            <div class="content-blocks">Seguro</div>
+          </div>
+        </li>
+        <li >
+          <div class="feature-box">
+            <div class="icon-support"></div>
+            <div class="content-blocks">Atención Al cliente</div>
+          </div>
+        </li>
+        <li>
+          <div class="feature-box">
+            <div class="icon-money"></div>
+            <div class="content-blocks">Garantía y Devolución</div>
+          </div>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+        <!-- /.info-boxes --> 
+        <!-- ============================================== INFO BOXES : END ============================================== --> 
+
+<!-- ============================================================= FOOTER ============================================================= -->
+
+<?php include('footer.html'); ?>
+
+<!-- ============================================================= FOOTER : END============================================================= -->
+</body>
+
+</html>
