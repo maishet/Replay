df66e358b8d7f40542f292dca87e832d84e000e3
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2022-01-03 03:50:16 -0500

    avance detalles del producto

--- assets/js/detallesProducto.js
+++ assets/js/detallesProducto.js
@@ -1,14 +1,10 @@
 $(document).ready(function() {
-
-	//On Click Event
 	$(".tabs .title").click(function() {    
-
-		$(".tabs .title").removeClass("active"); //Remove any "active" class
-		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tabs .title").removeClass("active");
+		$(this).addClass("active"); 
     $(".tabs .tabs-panel").hide();
-
-		var activeTab = $(this).attr("href"); //Find the href attribute value to identify the active tab + content
-		$(activeTab).show(); //Fade in the active ID content
+		var activeTab = $(this).attr("href"); 
+		$(activeTab).show(); 
 		return false;
 	});
 });0 
