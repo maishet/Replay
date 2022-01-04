e4b4001a5a1e77801e9372bcfd8531b14d96a4eb
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2022-01-04 02:57:39 -0500

    .

--- assets/js/detallesProducto.js
+++ assets/js/detallesProducto.js
@@ -8,3 +8,15 @@ $(document).ready(function() {
 		return false;
 	});
 });0 
+function sumar(){
+	let input=Number(document.getElementById("input-number").value);
+	input++;
+	document.getElementById("input-number").value=String(input);
+}
+function restar(){
+	let input=Number(document.getElementById("input-number").value);
+	if(input>1){
+		input--;
+	}
+	document.getElementById("input-number").value=String(input);
+}
