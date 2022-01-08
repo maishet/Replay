4a354950acc031b8792a4558ab23340875c316c6
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2022-01-07 21:43:22 -0500

    actualización comentarios

--- assets/js/detallesProducto.js
+++ assets/js/detallesProducto.js
@@ -10,6 +10,7 @@
 });0 
 document.getElementById("sumar").addEventListener("click",sumar);
 document.getElementById("restar").addEventListener("click",restar);
+//Seccion de compra
 function sumar(){
 	let input=Number(document.getElementById("input-number").value);
 	input++;
@@ -23,6 +24,22 @@ function restar(){
 	document.getElementById("input-number").value=String(input);
 }  
 
+//seccion comentarios
+var contsi,contno;
+function sumarSi(item){
+	let id=item.id[0];
+	contsi=1;
+	contno=0;
+	document.getElementById((id)+"comentarios-si").innerHTML="Si:"+String(contsi);
+	document.getElementById((id)+"comentarios-no").innerHTML="No:"+String(contno);
+}
+function sumarNo(item){
+	let id=item.id[0];
+	contno=1;
+	contsi=0;
+	document.getElementById((id)+"comentarios-no").innerHTML="No:"+String(contno);
+	document.getElementById((id)+"comentarios-si").innerHTML="Si:"+String(contsi);
+}
 //ventana comentario
 
 document.getElementById("btn-abrir1").addEventListener("click",abrir);
@@ -32,9 +49,33 @@ document.getElementById("pop-up").addEventListener("click",cerrar);
 
 function abrir(){
 	document.getElementById("ventana-comentario").style.display = "block";
-	 document.getElementById("pop-up").style.display = "block";
+	document.getElementById("pop-up").style.display = "block";
 }
 function cerrar(){
 	document.getElementById("ventana-comentario").style.display = "none";
 	document.getElementById("pop-up").style.display = "none";
+	for(let i=0;i<5;i++){
+		document.getElementById((i+1)+"estrella").style.color="#c4c4c4";
+	    document.getElementById((i+1)+"estrella").style.backgroundColor="white";
+	}
+	document.getElementById("calificacion").innerHTML="(0)";
+}
+var contador;
+function calificar(item){
+	let contadorEstrellas=0;
+	contador=item.id[0];
+	let nombre=item.id.substring(1);
+
+	for(let i=0;i<5;i++){
+		if(i<contador-1){
+			document.getElementById((i+1)+nombre).style.color="#c4c4c4";
+			document.getElementById((i+1)+nombre).style.backgroundColor="white";
+		}else{
+			document.getElementById((i+1)+nombre).style.color="white";
+			document.getElementById((i+1)+nombre).style.backgroundColor="#eebf61";
+			contadorEstrellas++
+		}
+	}
+	console.log("la calificacion es: "+contadorEstrellas);
+	document.getElementById("calificacion").innerHTML="("+String(contadorEstrellas)+")";
 }
