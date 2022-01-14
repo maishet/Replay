89729a003f2c3ff9b041d00b56e565fb515d12c0
unknown <moi.982555147@gmail.com>
2022-01-14 15:35:47 -0500

    arreglando más bugs xd

--- my-wishlist.php
+++ my-wishlist.php
@@ -76,37 +76,14 @@ else{
 		<table id="lista_deseos" class="table"> <!-- la tabla para colocar los productos -->
 			<thead>
 				<tr>
-					<th colspan="4" class="heading-title">My Lista de Deseos</th>
+					<th colspan="4" class="heading-title">Mi Lista de Deseos</th>
 				</tr>
 			</thead>
 			<tbody>
-<!-- 				<tr>
-					<td class="col-md-2 col-sm-6 col-xs-6"><img src="assets/images/products/p1.jpg" alt="imga"></td>
-					<td class="col-md-7 col-sm-6 col-xs-6">
-						<div class="product-name"><a href="#">Floral Print Buttoned </a></div>
-						<div class="rating">
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star rate"></i>
-							<i class="fa fa-star non-rate"></i>
-							<span class="review">( 06 Reviews )</span>
-						</div>
-						<div class="price">
-							$400.00
-							<span>$900.00</span>
-						</div>
-					</td>
-					<td class="col-md-2 ">
-						<a href="#" class="btn-upper btn btn-primary">Add to cart</a>
-					</td>
-					<td class="col-md-1 close-btn">
-						<a href="#" class=""><i class="fa fa-times"></i></a>
-					</td>
-				</tr> -->
+
 			</tbody>
 		</table>
-    <tfoot>No tiene productos en su Lista de Deseos</tfoot>
+    	<tfoot></tfoot>
 	</div>
 </div>			</div><!-- /.row -->
 		</div><!-- /.sigin-in-->
